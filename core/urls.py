52b9fcd092ad264b115134b4cda99349eee038ae
Murendeni <mnevondo727@gmail.com>
2021-11-28 20:17:19 +0200

    Add Survey Results Page

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from survey.views import index
 
 urlpatterns = [
+    path('', index, name='home'),
+    path('home/', index, name='home'),
     path('', include("survey.urls")),
     path('admin/', admin.site.urls),
 ]
+
+
--- survey/views.py
+++ survey/views.py
@@ -23,8 +23,74 @@ def take_survey(request):
 
 
 def survey_results(request):
+    sum_ages = 0
+    pizza_likes_count = 0
+    pasta_likes_count = 0
+    pap_and_wors_likes_count = 0
+
+    surveys = Survey.objects.filter(is_deleted=0)
+    surveys_count = Survey.objects.all().count()
+    for survey in surveys:
+        sum_ages += survey.age
+
+    oldest_person = surveys = Survey.objects.filter(is_deleted=0).order_by('-age')
+    youngest_person = surveys = Survey.objects.filter(is_deleted=0).order_by('age')
+    
+    fav_foods = Survey.objects.filter(is_deleted=0).values('favourite_food')
+    for food in fav_foods:
+        if 'Pizza' in food['favourite_food']:
+            pizza_likes_count += 1
+
+        if 'Pasta' in food['favourite_food']:
+            pasta_likes_count += 1
+
+        if 'Pap and Wors' in food['favourite_food']:
+            pap_and_wors_likes_count += 1
+
+    eats_out_rating_total = 0
+    eats_out_rating_total_diviser = 0
+
+    eats_out_ratings = Survey.objects.filter(is_deleted=0).values('likes_to_eat_out')
+    for rating in eats_out_ratings:
+        eats_out_rating_total_diviser += eats_out_ratings.count()
+        eats_out_rating_total+=rating['likes_to_eat_out']
+
+    watch_movies_rating_total = 0
+    watch_movies_rating_total_diviser = 0
+
+    watch_movies_ratings = Survey.objects.filter(is_deleted=0).values('likes_to_watch_movies')
+    for rating in watch_movies_ratings:
+        watch_movies_rating_total_diviser += watch_movies_ratings.count()
+        watch_movies_rating_total+=rating['likes_to_watch_movies']
+
+    watch_tv_rating_total = 0
+    watch_tv_rating_total_diviser = 0
+
+    watch_tv_ratings = Survey.objects.filter(is_deleted=0).values('likes_to_watch_tv')
+    for rating in watch_tv_ratings:
+        watch_tv_rating_total_diviser += watch_tv_ratings.count()
+        watch_tv_rating_total+=rating['likes_to_watch_tv']
+
+    listens_to_radi_rating_total = 0
+    listens_to_radi_rating_total_diviser = 0
+
+    listens_to_radio_rating = Survey.objects.filter(is_deleted=0).values('likes_listening_to_radio')
+    for rating in listens_to_radio_rating:
+        listens_to_radi_rating_total_diviser += listens_to_radio_rating.count()
+        listens_to_radi_rating_total+=rating['likes_listening_to_radio']
+
     return render(request, 'survey/survey_results.html', {
-        'name': 'Survey Results'
+        'surveys': surveys_count,
+        'average_age': round(sum_ages/ surveys_count, 1),
+        'olderst_person': oldest_person[0].age,
+        'youngest_person': youngest_person[0].age,
+        'pizza_likes_percentage':round((pizza_likes_count / surveys_count)*100,1),
+        'pasta_likes_percentage':round((pasta_likes_count / surveys_count)*100,1),
+        'pap_and_wors_likes_percentage':round((pap_and_wors_likes_count / surveys_count)*100,1),
+        'eats_out_average':round((eats_out_rating_total / eats_out_rating_total_diviser)*100,1),
+        'average_watch_movies_rating':round((watch_movies_rating_total / watch_movies_rating_total_diviser)*100,1),
+        'average_watch_tv_rating':round((watch_tv_rating_total / watch_tv_rating_total_diviser)*100,1),
+        'average_listens_to_radio_rating':round((listens_to_radi_rating_total / listens_to_radi_rating_total_diviser)*100,1),
     })
 
 def submit_survey(request):
